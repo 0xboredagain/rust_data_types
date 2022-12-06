cc7c991d564b811190c56dffe82bc803582ef723
0xboredagain <96192463+0xboredagain@users.noreply.github.com>
2022-12-06 23:32:19 +0530

    rust data types

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![deny(clippy::all)]
+
+fn main() {
+    let first_name = "foo";
+    let mut last_name = "bar";
+    let age = 20u8;
+    let population = 62_000_000;
+
+    println!("hi {} {} of {} years of age", first_name, last_name, age);
+
+    last_name = "baz";
+
+    println!("hi {} {} of {} years of age", first_name, last_name, age)
+}
+
+//data types
+//types of data
